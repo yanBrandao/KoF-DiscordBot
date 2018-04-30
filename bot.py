b51e41b3d5bd9367a82e464426af9996c17fa1f3
Lab01PC12\Prof. Yan <ybrandao.d@gmail.com>
2018-04-30 10:57:31 -0400

    [bot.py] Removed Token [bot.py] Changed 'entrar' command without parameter [.gitignore] Added *.token to remove tokens from project [kof_db] Changed database tables, pk and fk

--- bot.py
+++ bot.py
@@ -24,16 +24,16 @@ async def on_ready():
 
 @bot.event
 async def on_member_update(before, after):
-		print('{0.nick} encerrou {0.game}'.format(before))
 		game = '{0.game}'.format(after)
 		nick = '{0.nick}'.format(after)
-		print('{0.nick} iniciou {0.game}'.format(after))
 		if game != 'None':
+			print('{0.nick} iniciou {0.game}'.format(after))
 			userConnect[nick] = time.time()
 			print(userConnect)
 		game = '{0.game}'.format(before)
 		nick = '{0.nick}'.format(before)
 		if game != 'None':
+			print('{0.nick} encerrou {0.game}'.format(before))
 			timeSpend = time.time() - userConnect[nick]
 			userConnect.__delitem__(nick)
 			print(timeSpend)
@@ -46,16 +46,21 @@ async def on_voice_state_update(before, after):
 
 
 @bot.command(pass_context=True)
-async def entrar(self, member: discord.Member):
+async def entrar(self):
     user = discord.User
-
-    con = DBConnection()
-    sql = 'INSERT INTO public.player(nickname, victories, defeats, country, "discordUser") VALUES (\'{0.name}\', 0, 0, \'{0.server.region}\',\'{0.id}\');'.format(member)
-    print(sql)
-    if(con.changeDatabase(sql)):
-       await bot.say("Jogador inserido com sucesso ao rank :muscle:")
+    name = '{0.name}'.format(user)
+    member = self.message.author
+    nickname = '{0.nick}'.format(member)
+    if nickname == 'None':
+        await bot.say("Por favor, {} mude seu nickname no discord antes de solicitar entrada no Ranking. :)".format(name))
     else:
-        await bot.say("Você já foi adicionado ao ranking! :rolling_eyes:")
+        con = DBConnection()
+        sql = 'INSERT INTO public.player(nickname, victories, defeats, country, "discordUser") VALUES (\'{0.name}\', 0, 0, \'{0.server.region}\',\'{0.id}\');'.format(member)
+        print(sql)
+        if(con.changeDatabase(sql)):
+            await bot.say("Jogador inserido com sucesso ao rank :muscle:")
+        else:
+            await bot.say("Você já foi adicionado ao ranking! :rolling_eyes:")
     
 
 
@@ -94,8 +99,10 @@ async def duelo(ctx, user: discord.Member):
     await bot.say('Lutem!')
 
 
+file = open('bot.token', 'r')
 
+token = file.read()
 
+file.close()
 
-
-bot.run("NDM2NzMzMjg3NjU5MjA4NzEy.DbsKTg.aHBJh8_eapGtLNBCv_84rw1lirQ")
+bot.run(token)
