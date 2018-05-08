8943802df39b832537587eca7918b2870f49e9a5
Lab01PC01\Super <ybrandao.d@gmail.com>
2018-05-08 08:02:17 -0400

    [Django-Web] Added Mainpage

--- Django-Web/kof_database/database/views.py
+++ Django-Web/kof_database/database/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Kof-Database")
+	template = loader.get_template('database/index.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
